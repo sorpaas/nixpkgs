df922f1d2166cdce61441dfa63b0f9a7b60e29db
Wei Tang <hi@that.world>
2016-05-07 17:00:03 +0200

    merge-conflict: Use kdeFramework from master

--- pkgs/desktops/kde-5/frameworks-5.21/kinit/default.nix
+++ pkgs/desktops/kde-5/frameworks-5.21/kinit/default.nix
@@ -4,10 +4,11 @@
 
 kdeFramework {
   name = "kinit";
-  meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules kdoctools libcap_progs ];
-  propagatedBuildInputs = [
-    kconfig kcrash ki18n kio kservice kwindowsystem libcap
-  ];
+  nativeBuildInputs = [ extra-cmake-modules kdoctools libcap.out ];
+  buildInputs = [ kconfig kcrash kservice libcap ];
+  propagatedBuildInputs = [ ki18n kio kwindowsystem ];
   patches = copyPathsToStore (lib.readPathsFromFile ./. ./series);
+  meta = {
+    maintainers = [ lib.maintainers.ttuegel ];
+  };
 }
