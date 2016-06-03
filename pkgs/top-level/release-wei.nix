a915efae426fbd9dd5cacf68b1c2889871475297
Wei Tang <hi@that.world>
2016-06-03 11:16:05 +0200

    Update customized packages

--- pkgs/top-level/release-wei.nix
+++ pkgs/top-level/release-wei.nix
@@ -1,8 +1,109 @@
 { nixpkgs ? { outPath = (import ./all-packages.nix {}).lib.cleanSource ../..; revCount = 1234; shortRev = "abcdef"; }
-, supportedSystems ? [ "x86_64-linux" "i686-linux" ]
+, supportedSystems ? [ "x86_64-linux" ]
+, packageSet ? (import ./../..)
+, allowTexliveBuilds ? false
+, scrubJobs ? true
 }:
 
-with import ./release-lib.nix { inherit supportedSystems; };
+with import ../../lib;
+
+with rec {
+
+  # Ensure that we don't build packages marked as unfree.
+  allPackages = args: packageSet (args // {
+    config.allowUnfree = true;
+    config.allowTexliveBuilds = allowTexliveBuilds;
+    config.inHydra = true;
+  });
+
+  pkgs = pkgsFor "x86_64-linux";
+
+  hydraJob' = if scrubJobs then hydraJob else id;
+
+  /* !!! Hack: poor man's memoisation function.  Necessary to prevent
+     Nixpkgs from being evaluated again and again for every
+     job/platform pair. */
+  pkgsFor = system:
+    if system == "x86_64-linux" then pkgs_x86_64_linux
+    else if system == "i686-linux" then pkgs_i686_linux
+    else if system == "x86_64-darwin" then pkgs_x86_64_darwin
+    else if system == "x86_64-freebsd" then pkgs_x86_64_freebsd
+    else if system == "i686-freebsd" then pkgs_i686_freebsd
+    else if system == "i686-cygwin" then pkgs_i686_cygwin
+    else if system == "x86_64-cygwin" then pkgs_x86_64_cygwin
+    else abort "unsupported system type: ${system}";
+
+  pkgs_x86_64_linux = allPackages { system = "x86_64-linux"; };
+  pkgs_i686_linux = allPackages { system = "i686-linux"; };
+  pkgs_x86_64_darwin = allPackages { system = "x86_64-darwin"; };
+  pkgs_x86_64_freebsd = allPackages { system = "x86_64-freebsd"; };
+  pkgs_i686_freebsd = allPackages { system = "i686-freebsd"; };
+  pkgs_i686_cygwin = allPackages { system = "i686-cygwin"; };
+  pkgs_x86_64_cygwin = allPackages { system = "x86_64-cygwin"; };
+
+
+  /* The working or failing mails for cross builds will be sent only to
+     the following maintainers, as most package maintainers will not be
+     interested in the result of cross building a package. */
+  crossMaintainers = [ ];
+
+
+  /* Build a package on the given set of platforms.  The function `f'
+     is called for each supported platform with Nixpkgs for that
+     platform as an argument .  We return an attribute set containing
+     a derivation for each supported platform, i.e. ‘{ x86_64-linux =
+     f pkgs_x86_64_linux; i686-linux = f pkgs_i686_linux; ... }’. */
+  testOn = systems: f: genAttrs
+    (filter (x: elem x supportedSystems) systems) (system: hydraJob' (f (pkgsFor system)));
+
+
+  /* Similar to the testOn function, but with an additional
+     'crossSystem' parameter for allPackages, defining the target
+     platform for cross builds. */
+  testOnCross = crossSystem: systems: f: {system ? builtins.currentSystem}:
+    if elem system systems
+    then f (allPackages { inherit system crossSystem; })
+    else {};
+
+
+  /* Given a nested set where the leaf nodes are lists of platforms,
+     map each leaf node to `testOn [platforms...] (pkgs:
+     pkgs.<attrPath>)'. */
+  mapTestOn = mapAttrsRecursive
+    (path: systems: testOn systems (pkgs: getAttrFromPath path pkgs));
+
+
+  /* Similar to the testOn function, but with an additional 'crossSystem'
+   * parameter for allPackages, defining the target platform for cross builds,
+   * and triggering the build of the host derivation (cross built - crossDrv). */
+  mapTestOnCross = crossSystem: mapAttrsRecursive
+    (path: systems: testOnCross crossSystem systems
+      (pkgs: addMetaAttrs { maintainers = crossMaintainers; } (getAttrFromPath path pkgs)));
+
+
+  /* Recursively map a (nested) set of derivations to an isomorphic
+     set of meta.platforms values. */
+  packagePlatforms = mapAttrs (name: value:
+    let res = builtins.tryEval (
+      if isDerivation value then
+        value.meta.hydraPlatforms or (value.meta.platforms or [])
+      else if value.recurseForDerivations or false || value.recurseForRelease or false then
+        packagePlatforms value
+      else
+        []);
+    in if res.success then res.value else []
+    );
+
+
+  /* Common platform groups on which to test packages. */
+  inherit (platforms) unix linux darwin cygwin allBut all mesaPlatforms;
+
+  /* Platform groups for specific kinds of applications. */
+  x11Supported = linux;
+  gtkSupported = linux;
+  ghcSupported = linux;
+
+};
 
 {
 
@@ -16,7 +117,6 @@ with import ./release-lib.nix { inherit supportedSystems; };
   aspell = linux;
   at = linux;
   atlas = linux;
-  aterm25 = linux;
   autoconf = linux;
   automake = linux;
   avahi = linux;
@@ -28,7 +128,6 @@ with import ./release-lib.nix { inherit supportedSystems; };
   bumblebee = linux;
   bsdiff = linux;
   bzip2 = linux;
-  classpath = linux;
   cmake = linux;
   coreutils = linux;
   cpio = linux;
@@ -46,7 +145,6 @@ with import ./release-lib.nix { inherit supportedSystems; };
   gcj = linux;
   glibc = linux;
   glibcLocales = linux;
-  gnat = linux;
   gnugrep = linux;
   gnum4 = linux;
   gnumake = linux;
@@ -86,7 +184,6 @@ with import ./release-lib.nix { inherit supportedSystems; };
   libxml2 = linux;
   libxslt = linux;
   lout = linux;
-  lsh = linux;
   lsof = linux;
   ltrace = linux;
   lvm2 = linux;
@@ -153,13 +250,9 @@ with import ./release-lib.nix { inherit supportedSystems; };
   sysvtools = linux;
   tcl = linux;
   tcpdump = linux;
-  texLive = linux;
-  texLiveBeamer = linux;
-  texLiveExtra = linux;
   texinfo = linux;
   time = linux;
   tinycc = linux;
-  udev = linux;
   unrar = linux;
   unzip = linux;
   upstart = linux;
@@ -178,11 +271,4 @@ with import ./release-lib.nix { inherit supportedSystems; };
   xkeyboard_config = linux;
   zile = linux;
   zip = linux;
-
-  dbus = {
-    libs = linux;
-    daemon = linux;
-    tools = linux;
-  };
-
 } ))
