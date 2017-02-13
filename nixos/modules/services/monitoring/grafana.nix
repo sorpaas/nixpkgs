933bdd9008f72670411626a42d3f6057a3f42449
Rickard Nilsson <rickynils@gmail.com>
2017-02-13 23:11:40 +0100

    nixos/grafana: Don't print password warning if no password has been set

--- nixos/modules/services/monitoring/grafana.nix
+++ nixos/modules/services/monitoring/grafana.nix
@@ -239,8 +239,6 @@ in {
 
     environment.systemPackages = [ cfg.package ];
 
-    environment.systemPackages = [ cfg.package ];
-
     systemd.services.grafana = {
       description = "Grafana Service Daemon";
       wantedBy = ["multi-user.target"];
